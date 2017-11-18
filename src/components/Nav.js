737cac27c2b74ccb23387cb048cb98cf007dad4c
amite <amit.erandole@gmail.com>
2017-11-18 11:58:04 +0530

    added selected active class to tab

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,10 @@ const Nav = ({ activeTab, onTabChange }) => {
   return (
     <nav className="App-nav">
       <ul>
-        <li className="App-nav-item">
+        <li className={`App-nav-item ${activeTab === 0 && 'selected'}`}>
           <a onClick={() => onTabChange(0)}>Items</a>
         </li>
-        <li className="App-nav-item">
+        <li className={`App-nav-item ${activeTab === 1 && 'selected'}`}>
           <a onClick={() => onTabChange(1)}>Cart</a>
         </li>
       </ul>
