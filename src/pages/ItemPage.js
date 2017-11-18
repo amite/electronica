a87b609319468424867f84a9c1b50f6e91109623
amite <amit.erandole@gmail.com>
2017-11-18 16:07:33 +0530

    added loader component

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -2,19 +2,26 @@ import React from 'react'
 import Item from '../components/Item'
 import PropTypes from 'prop-types'
 import './ItemPage.css'
+import Loader from '../components/Loader'
 
 function ItemPage({ items, loading }) {
-  return (
-    <ul className="ItemPage-items">
-      {items.map(item => (
-        <li key={item.key} className="ItemPage-item">
-          <Item item={item}>
-            <button className="Item-addToCart">Add to Cart</button>
-          </Item>
-        </li>
-      ))}
-    </ul>
-  )
+  let content
+  if (loading) {
+    content = <Loader />
+  } else {
+    content = (
+      <ul className="ItemPage-items">
+        {items.map(item => (
+          <li key={item.key} className="ItemPage-item">
+            <Item item={item}>
+              <button className="Item-addToCart">Add to Cart</button>
+            </Item>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  return <div>{content}</div>
 }
 
 ItemPage.propTypes = {
