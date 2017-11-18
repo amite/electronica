ff8b78f72116840c10bd0b0de3ade0b3702cf780
amite <amit.erandole@gmail.com>
2017-11-18 09:57:11 +0530

    added page bodies

--- src/App.js
+++ src/App.js
@@ -7,6 +7,43 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
+        <main className="App-content">
+          <ul className="ItemPage-items">
+            <li className="ItemPage-item">
+              <div className="Item">
+                <div className="Item-left">
+                  <img className="Item-image" />
+                  <div className="Item-title">Product Name</div>
+                  <div className="Item-description">Description</div>
+                </div>
+                <div className="Item-right">
+                  <div className="Item-price">Price</div>
+                  <button className="Item-addToCart">Add to Cart</button>
+                </div>
+              </div>
+            </li>
+          </ul>
+
+          <ul className="CartPage-items">
+            <li className="CartPage-item">
+              <div className="Item">
+                <div className="Item-left">
+                  <img className="Item-image" src="" />
+                  <div className="Item-title">Product Name</div>
+                  <div className="Item-description">Description</div>
+                </div>
+                <div className="Item-right">
+                  <div className="Item-price">Price</div>
+                  <div className="CartItem-controls">
+                    <button className="CartItem-removeOne">&ndash;</button>
+                    <span className="CartItem-count">Count</span>
+                    <button className="CartItem-addOne">+</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </main>
       </div>
     )
   }
