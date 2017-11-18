91ddc65a1916fa3589b4942e4ddf467c14ce64b4
amite <amit.erandole@gmail.com>
2017-11-18 08:24:54 +0530

    added top level navigation

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="App-nav">
+          <ul>
+            <li className="App-nav-item">
+              <a>Items</a>
+            </li>
+            <li className="App-nav-item">
+              <a>Cart</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     )
   }
